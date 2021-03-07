7d085d52fa0a83e43fb009d8794096d14f33549a
alihoppabb <78644878+alihoppabb@users.noreply.github.com>
2021-03-07 21:34:34 +0300

    changed

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,25 @@
   <div class="home">
   hello2
   </div>
+  <div>
+    <nav>
+      <ul>
+        <li>rot</li>
+        <li>git</li>
+        <li>ebal</li>
+      </ul>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
